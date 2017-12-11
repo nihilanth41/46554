0d5714a61f7b50fd6bd65616910d85e78dd9c6fa
Zachary Rump <zrrm74@mail.mizzou.edu>
2017-12-10 19:04:31 -0600

    test detector

--- src/detection.cpp
+++ src/detection.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -13,16 +15,26 @@ using namespace std;
  - Moar sample data (see README)
 */
 
-int main(int argc, char **argv) {
-	if(argc != 2) {
-		cout << "Usage: ./detection.bin <path/to/image>" << endl;
-		return -1;
+void testDetector(void);
+void detection(char *);
+
+void testDetector(void) {
+	static int i;
+	static int image_max = 460;
+	char buf[512];
+	for(i=1; i<image_max+1; i++) {
+		sprintf(buf, "../persons/person_%03d.bmp", i);
+		cout << "Opening file: " << buf << endl;
+		detection(buf);
 	}
+}
+
+void detection(char *image_path) {
 	Mat image;
-	image = imread(argv[1], CV_LOAD_IMAGE_COLOR); // Read file
+	image = imread(image_path, CV_LOAD_IMAGE_COLOR); // Read file
 	if(! image.data) {
-		cout << "Could not open image: " << argv[1] <<  endl;
-		return -2;
+		cout << "Could not open image: " << image_path <<  endl;
+		exit(-2);
 	}
 
 	/* http://answers.opencv.org/question/22420/human-detection-with-hog-how-to-improve-it/ */
@@ -33,8 +45,8 @@ int main(int argc, char **argv) {
 	/* For holding the objects */
 	vector<Rect> found, found_filtered; 
 
-	/* Input image, object_boundaries, hit_threshold, window_stride, padding,  */
-	hog.detectMultiScale(image, found, 0, Size(8,8), Size(32,32), 1.05, 2);
+	/* args: image, foundlocations, hit_thresh, winStride, Padding, scale, finalthresh, usemeanshift grouping bool */
+	hog.detectMultiScale(image, found, 0, Size(8,8), Size(32,32), 1.05, 3);
 	size_t i, j;
 	for (i=0; i<found.size(); i++)
 	{
@@ -45,6 +57,8 @@ int main(int argc, char **argv) {
 		if (j==found.size())
 			found_filtered.push_back(r);
 	}
+
+	cout << "Number of people detected: " << found_filtered.size() << endl;
 	for (i=0; i<found_filtered.size(); i++)
 	{
 		Rect r = found_filtered[i];
@@ -55,9 +69,19 @@ int main(int argc, char **argv) {
 		rectangle(image, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
 	}
 
-	namedWindow("Display window", WINDOW_AUTOSIZE);
-	imshow("Display window", image);
-	waitKey(0);
+	if(found_filtered.size() > 0) {
+		namedWindow("Display window", WINDOW_AUTOSIZE);
+		imshow("Display window", image);
+		waitKey(0);
+	}
+}
+
+int main(int argc, char **argv) {
+	if(argc != 2) {
+		cout << "Usage: ./detection.bin <path/to/image>" << endl;
+		return -1;
+	}
+	testDetector();
 
 	return EXIT_SUCCESS;
 }
